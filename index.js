2a6991ab45c21389bbcd351e9446818a9ffe1ab8
fc440 <fc440classes@gmail.com>
2022-06-02 07:39:32 -0400

    js tutorial

---
+++ index.js
@@ -0,0 +1,35 @@
+// This is my first javascript code
+
+
+/*let name = 'Frank';
+console.log('Frank');
+
+const interestRate = 0.3;
+console.log(interestRate);
+
+// Datatype examples
+
+Primitive Value Data Types
+let name = 'Frank';  // String Literal
+let age = 40; // Number Literal 
+let isApproved = true; // Boolean Literal
+let firstName = undefinded; // Undefined
+let lastName = null; //Null... we use nulll if we want to reset an option
+
+
+// Reference Data Types
+let person = {   //Object Literal can be grouped to condense code
+    name: 'Frank',    //Key Values of Objects
+    age: 40
+};
+
+console.log(person)
+
+let selectedColors = ['red', 'blue']; // Array Literal similar to dictionaries in python... first position is 0
+console.log(selectedColors);*/
+
+function greet() {     // Function Literal
+    console.log('Hello User');                           //logic of function
+}
+
+greet();
