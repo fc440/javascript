143386d19421e07dc71238d8620c88d0b7f62b5c
fc440 <fc440classes@gmail.com>
2022-06-03 21:06:10 -0400

    brocode js

---
+++ Bro_Code/index.js
@@ -0,0 +1,30 @@
+//console.log('This is a js test');
+//console.log("Hopefully this class will not be difficult")
+
+//window.alert('THIS IS ANOTHER TEST')
+
+// this is a comment
+/* this 
+is a 
+longer comment */
+
+// A variable is a container for storing data
+// A variable behaves as if it was the value that it contains
+
+
+// Two steps
+// 1. Declaration (var, let, const)
+// 2. Assignment ( = assignment operator)
+
+let firstName = 'Frank'; //string literal
+age = 21;  // number literal
+let student = true;  // boolean literal 
+
+
+console.log('Hello', firstName)
+console.log('You are', age, 'years old')
+console.log('Enrolled:', student)
+
+document.getElementById('p1').innerHTML = "Hello " + firstName;
+document.getElementById('p2').innerHTML = "You are " + age + " years old";
+document.getElementById('p3').innerHTML = "Enrolled: " + student;
